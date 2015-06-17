06268c4b35935ee042ec4c1497faec7cb0443aeb
badview08 <jonesgabriel21@gmail.com>
2015-06-17 15:18:31 -0400

    title has been created

---
+++ dist/scripts/templates.js
@@ -0,0 +1,7 @@
+this["JST"] = this["JST"] || {};
+this["JST"]["application"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    return "<h1>Hello</h1>\n";
+},"useData":true});
+this["JST"]["menu"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    return "<header>\n  <h2>Thai and Tie</h2>\n</header>\n";
+},"useData":true});
