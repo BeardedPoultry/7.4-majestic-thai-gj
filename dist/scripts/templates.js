cb218824230ef111ecf6bc1f0bfa515a81ca3f25
badview08 <jonesgabriel21@gmail.com>
2015-06-18 09:03:25 -0400

    created menu template

--- dist/scripts/templates.js
+++ dist/scripts/templates.js
@@ -6,7 +6,7 @@ this["JST"]["hours"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"ma
     return "";
 },"useData":true});
 this["JST"]["menu"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    return "<header>\n  <h2>Thai and Tie</h2>\n</header>\n<div>\n  \n";
+    return "<header>\n  <i class=\"fa fa-cutlery fa-3\"></i>\n  <h2>Thai and Tie</h2>\n  <div></div>\n  <p>Food Delivery & Takeout<p>\n    <button class=\"home-button\" placeholder=\"Home\">HOME</button>\n  <button class=\"support-button\" placeholder=\"Home\">SUPPORT</button>\n</header>\n<div class=\"nameofrest\">\n  <h1>Taste of a Spicy Dish</h1>\n  <span>We are located at:</span>\n  <span>Edgegrove Chase. A semi detached villa in a private cul-de-sac in the suburbs of the city.</span>\n  <label for=\"menu\">Menu</label>\n  <label for=\"hours-info\">Hours & Info</label>\n  <label for=\"reviews\">Reviews</label>\n\n</div>\n";
 },"useData":true});
 this["JST"]["reviews"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     return "";
